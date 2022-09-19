fbc027ab20405536ba3e6897961daafbc890adc8
Anthony Ng <engineer.anthony.ng@gmail.com>
2022-09-19 09:13:50 +0100

    Add read csv logic and file

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ const path = require("path");
 const port = 80;
 
 app.get("/data", (req, res) => {
-    // console.log("/data is called, path = ", path.join(__dirname, "/data.csv"));
-    // fs.readFile(
-    //   path.join(__dirname, "/data.csv"),
-    //   { encoding: "utf-8" },
-    //   (err, f) => {
-    //     if (err) console.log(err);
+    console.log("/data is called, path = ", path.join(__dirname, "/data.csv"));
+    fs.readFile(
+      path.join(__dirname, "/data.csv"),
+      { encoding: "utf-8" },
+      (err, f) => {
+        if (err) console.log(err);
   
-    //     res.setHeader("Access-Control-Allow-Origin", "*");
-    //     res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-    //     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    //     res.send(f);
-    //   }
-    // );
-    res.send({
-        'message': 'hello vercel'
-    })
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.send(f);
+      }
+    );
   });
 
 app.listen(port, () => {
